addfa4b22c38d6862de08bb55027ff26c05b7428
NA <nhatanhtruong20@gmail.com>
2025-06-02 14:22:39 +0700

    feat: add Endpoints API Explorer to Swagger configuration

--- PsychologySupport/Services/LifeStyles/LifeStyles.API/Extensions/ApplicationServiceExtensions.cs
+++ PsychologySupport/Services/LifeStyles/LifeStyles.API/Extensions/ApplicationServiceExtensions.cs
@@ -45,6 +45,7 @@ public static class ApplicationServiceExtensions
 
     private static void ConfigureSwagger(IServiceCollection services)
     {
+        services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(options =>
         {
             options.SwaggerDoc("v1", new OpenApiInfo
