92835b7be9f516f671b32739256b52945d6336ea
Nhat Anh <nhatanhtruong20@gmail.com>
2025-06-03 11:14:46 +0700

    feat: enable Kestrel server configuration for HTTPS with certificate setup

--- PsychologySupport/ApiGateways/YarpApiGateway/Program.cs
+++ PsychologySupport/ApiGateways/YarpApiGateway/Program.cs
@@ -3,22 +3,22 @@ using YarpApiGateway.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.WebHost.ConfigureKestrel(serverOptions =>
-// {
-//     serverOptions.ConfigureHttpsDefaults(httpsOptions =>
-//     {
-//         httpsOptions.ServerCertificate = X509Certificate2.CreateFromPemFile(
-//             "/certs/fullchain1.pem",
-//             "/certs/privkey1.pem"
-//         );
-//     });
-//
-//     serverOptions.ListenAnyIP(80);
-//     serverOptions.ListenAnyIP(443, listenOptions =>
-//     {
-//         listenOptions.UseHttps();
-//     });
-// });
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ConfigureHttpsDefaults(httpsOptions =>
+    {
+        httpsOptions.ServerCertificate = X509Certificate2.CreateFromPemFile(
+            "/certs/fullchain1.pem",
+            "/certs/privkey1.pem"
+        );
+    });
+
+    serverOptions.ListenAnyIP(80);
+    serverOptions.ListenAnyIP(443, listenOptions =>
+    {
+        listenOptions.UseHttps();
+    });
+});
 
 
 builder.Services.AddApplicationServices(builder.Configuration);
