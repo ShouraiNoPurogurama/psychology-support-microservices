807035ad2ae8da87c620f5d2a3d45072e2488886
Nhat Anh <nhatanhtruong20@gmail.com>
2025-06-06 23:09:23 +0700

    fix pagination logic in GetAllQuestionOptionsHandler to correctly calculate skip index

--- PsychologySupport/Services/Test/Test.Application/Tests/Queries/GetAllTestQuestionsHandler.cs
+++ PsychologySupport/Services/Test/Test.Application/Tests/Queries/GetAllTestQuestionsHandler.cs
@@ -41,7 +41,6 @@ public class GetAllTestQuestionsHandler : IQueryHandler<GetAllTestQuestionsQuery
             totalCount,
             testQuestions
         );
-
         return new GetAllTestQuestionsResult(paginatedResult);
     }
 }
