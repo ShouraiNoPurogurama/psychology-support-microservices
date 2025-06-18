35fd807aea668df92eb5b85609f56ecce23b6973
nctan123 <canhtancanhtan@gmail.com>
2025-06-18 14:02:33 +0700

    refactor addPaymentCode

--- PsychologySupport/Services/Payment/Payment.Infrastructure/Data/Migrations/20250618070150_AddPaymentCode.Designer.cs
+++ PsychologySupport/Services/Payment/Payment.Infrastructure/Data/Migrations/20250618070150_AddPaymentCode.Designer.cs
@@ -12,7 +12,7 @@ using Payment.Infrastructure.Data;
 namespace Payment.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(PaymentDbContext))]
-    [Migration("20250617165833_AddPaymentCode")]
+    [Migration("20250618070150_AddPaymentCode")]
     partial class AddPaymentCode
     {
         /// <inheritdoc />
