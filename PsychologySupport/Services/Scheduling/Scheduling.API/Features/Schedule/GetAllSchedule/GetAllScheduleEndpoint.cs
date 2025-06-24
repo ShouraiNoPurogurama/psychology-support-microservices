a2ef854d0911dee32c45d632cf4c25b11c896827
nctan123 <canhtancanhtan@gmail.com>
2025-06-24 16:32:54 +0700

    refactor GetAllSchedule

--- PsychologySupport/Services/Scheduling/Scheduling.API/Features/Schedule/GetAllSchedule/GetAllScheduleEndpoint.cs
+++ PsychologySupport/Services/Scheduling/Scheduling.API/Features/Schedule/GetAllSchedule/GetAllScheduleEndpoint.cs
@@ -2,18 +2,28 @@
 using Carter;
 using Mapster;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Scheduling.API.Dtos;
 
 namespace Scheduling.API.Features.Schedule.GetAllSchedule
 {
+    public record GetAllSchedulesRequest(
+     int PageIndex,
+     int PageSize,
+     string? Search,
+     string? SortBy,
+     string? SortOrder,
+     Guid? DoctorId,
+     Guid? PatientId);
     public record GetAllSchedulesResponse(PaginatedResult<ScheduleDto> Schedules);
     public class GetAllScheduleEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/schedules", async ([AsParameters] GetAllSchedulesQuery request, ISender sender) =>
+            app.MapGet("/schedules", async ([AsParameters] GetAllSchedulesRequest request, ISender sender) =>
             {
-                var result = await sender.Send(request);
+                var query = request.Adapt<GetAllSchedulesQuery>();
+                var result = await sender.Send(query);
                 var response = result.Adapt<GetAllSchedulesResponse>();
                 return Results.Ok(response);
             })
