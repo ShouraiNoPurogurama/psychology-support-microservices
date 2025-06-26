9766144a7690d8f4dbadc954de130750f7613a71
nctan123 <canhtancanhtan@gmail.com>
2025-06-26 22:16:20 +0700

    setup rsa

---
+++ PsychologySupport/Services/Test/Test.Infrastructure/Data/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Infrastructure.Data.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMinimalJwtValidation(configuration);
+            return services;
+        }
+    }
+}
