140bb6f25edc63e87b3c92f2d5ffac57304ad32f
Nhat Anh <nhatanhtruong20@gmail.com>
2025-06-29 21:08:02 +0700

    feat: add PersonaSnapshot column to AIChatSessions and update related models

---
+++ PsychologySupport/Services/ChatBox/ChatBox.API/Data/Migrations/20250629135508_PersonaSnapshot.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatBox.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PersonaSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PersonaSnapshot",
+                schema: "public",
+                table: "AIChatSessions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersonaSnapshot",
+                schema: "public",
+                table: "AIChatSessions");
+        }
+    }
+}
