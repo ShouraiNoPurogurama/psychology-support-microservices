abd4f6cfd9a784ca03d58c7c3660f8fceb28e2f2
Nhat Anh <nhatanhtruong20@gmail.com>
2025-06-30 21:16:32 +0700

    feat: enhance GetAllImprovementGoalHandler to include translation of improvement goals

--- PsychologySupport/Services/LifeStyles/LifeStyles.API/Features/ImprovementGoal/GetAllImprovementGoal/GetAllImprovementGoalHandler.cs
+++ PsychologySupport/Services/LifeStyles/LifeStyles.API/Features/ImprovementGoal/GetAllImprovementGoal/GetAllImprovementGoalHandler.cs
@@ -1,7 +1,10 @@
 ﻿using BuildingBlocks.CQRS;
+using BuildingBlocks.Messaging.Events.Translation;
 using LifeStyles.API.Data;
 using LifeStyles.API.Dtos;
+using LifeStyles.API.Extensions;
 using Mapster;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 namespace LifeStyles.API.Features.ImprovementGoal.GetAllImprovementGoal;
@@ -10,15 +13,22 @@ public record GetAllImprovementGoalQuery() : IQuery<GetAllImprovementGoalResult>
 
 public record GetAllImprovementGoalResult(IEnumerable<ImprovementGoalDto> Goals);
 
-public class GetAllImprovementGoalHandler(LifeStylesDbContext context)
+public class GetAllImprovementGoalHandler(
+    LifeStylesDbContext context,
+    IRequestClient<GetTranslatedDataRequest> translatedDataClient)
     : IQueryHandler<GetAllImprovementGoalQuery, GetAllImprovementGoalResult>
 {
     public async Task<GetAllImprovementGoalResult> Handle(GetAllImprovementGoalQuery request, CancellationToken cancellationToken)
     {
         var goals = await context.ImprovementGoals.ToListAsync(cancellationToken);
 
-        var goalDtos = goals.Adapt<IEnumerable<ImprovementGoalDto>>();
+        var goalDtos = goals.Adapt<List<ImprovementGoalDto>>();
 
-        return new GetAllImprovementGoalResult(goalDtos);
+        var translatedGoals = await goalDtos.TranslateEntitiesAsync(nameof(Models.ImprovementGoal), translatedDataClient,
+            i => i.Id.ToString(), cancellationToken, 
+            i => i.Name, 
+            i => i.Description);
+
+        return new GetAllImprovementGoalResult(translatedGoals);
     }
 }
