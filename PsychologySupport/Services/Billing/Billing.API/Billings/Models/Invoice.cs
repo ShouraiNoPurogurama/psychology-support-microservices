59d02e7215ab6d20b434d3315089782f41800b99
nctan123 <canhtancanhtan@gmail.com>
2025-09-03 15:38:46 +0700

    feat: setup Billing module

---
+++ PsychologySupport/Services/Billing/Billing.API/Billings/Models/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Billing.API.Billings.Models;
+
+public partial class Invoice
+{
+    public Guid Id { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public Guid OrderId { get; set; }
+
+    public Guid AliasId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime IssuedAt { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public DateTime LastModified { get; set; }
+
+    public Guid LastModifiedBy { get; set; }
+
+    public virtual InvoiceSnapshot? InvoiceSnapshot { get; set; }
+
+    public virtual Order Order { get; set; } = null!;
+}
