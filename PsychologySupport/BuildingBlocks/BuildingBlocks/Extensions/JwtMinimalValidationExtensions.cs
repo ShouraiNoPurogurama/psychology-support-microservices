35583f065d50f7b55988631b03887499f7fac13c
Nhat Anh <nhatanhtruong20@gmail.com>
2025-09-11 22:29:09 +0700

    refactor(auth): remove redundant token validation logic

--- PsychologySupport/BuildingBlocks/BuildingBlocks/Extensions/JwtMinimalValidationExtensions.cs
+++ PsychologySupport/BuildingBlocks/BuildingBlocks/Extensions/JwtMinimalValidationExtensions.cs
@@ -1,14 +1,9 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using BuildingBlocks.Constants;
+﻿using System.Security.Cryptography;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using StackExchange.Redis;
 
 namespace BuildingBlocks.Extensions;
 
@@ -36,41 +31,6 @@ public static class JwtMinimalValidationExtensions
                     ValidateLifetime = true,
                     ClockSkew = TimeSpan.Zero,
                     IssuerSigningKey = new RsaSecurityKey(rsaKey)
-                };
-
-                options.Events = new JwtBearerEvents
-                {
-                    OnTokenValidated = async context =>
-                    {
-                        var redisConnection = context.HttpContext.RequestServices.GetRequiredService<IConnectionMultiplexer>();
-                        var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<JwtBearerEvents>>();
-
-                        var jti = context.Principal?.FindFirstValue(JwtRegisteredClaimNames.Jti);
-
-                        if (string.IsNullOrEmpty(jti))
-                        {
-                            logger.LogWarning("Token validation failed. JTI claim is missing.");
-                            context.Fail("Phiên đăng nhập đã hết hạn, vui lòng đăng nhập lại.");
-                            return;
-                        }
-
-                        var db = redisConnection.GetDatabase();
-                        var redisKey = MyStrings.GenerateRevokedTokenKey(jti);;
-
-                        try
-                        {
-                            if (await db.KeyExistsAsync(redisKey))
-                            {
-                                logger.LogInformation("Token validation failed. Token with JTI {Jti} has been revoked.", jti);
-                                context.Fail("This token has been revoked.");
-                            }
-                        }
-                        catch (RedisConnectionException ex)
-                        {
-                            logger.LogError(ex, "Could not connect to Redis. Deciding on fail-open/fail-closed strategy.");
-                            // context.Fail("Authentication service is unavailable.");
-                        }
-                    }
                 };
             });
     }
