874d457e8fe5037f8351e08fdca1315491ad06ad
Nhat Anh <nhatanhtruong20@gmail.com>
2025-10-03 17:41:28 +0700

    feat(posts): add media_url column to post_media for media replication

---
+++ PsychologySupport/Services/Post/Post.Infrastructure/Data/Post/Migrations/20251003103943_AddMediaUrlReplication.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Post.Infrastructure.Data.Post.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaUrlReplication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "media_url",
+                schema: "post",
+                table: "post_media",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "media_url",
+                schema: "post",
+                table: "post_media");
+        }
+    }
+}
