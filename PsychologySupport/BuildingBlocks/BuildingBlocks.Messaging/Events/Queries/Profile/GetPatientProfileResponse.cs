5ad9288a8a85fbacbb15934dfb5a03779fb6755a
nctan123 <canhtancanhtan@gmail.com>
2025-10-04 01:21:44 +0700

    feat: add GetPatientProfileRequestHandler to fetch patient info

--- PsychologySupport/BuildingBlocks/BuildingBlocks.Messaging/Events/Queries/Profile/GetPatientProfileResponse.cs
+++ PsychologySupport/BuildingBlocks/BuildingBlocks.Messaging/Events/Queries/Profile/GetPatientProfileResponse.cs
@@ -6,12 +6,12 @@ public record GetPatientProfileResponse(
     bool PatientExists,
     Guid Id,
     string FullName,
-    UserGender Gender,
-    string? Allergies,
-    string PersonalityTraits,
-    string Address,
+    //UserGender Gender,
+    //string? Allergies,
+    //string PersonalityTraits,
+    //string Address,
     string PhoneNumber,
     string Email,
-    Guid UserId,
-    bool IsProfileCompleted
+    Guid UserId
+    //bool IsProfileCompleted
     );
