a2f0d236ff0c83e4d46d0660759776242e288f92
Nhat Anh <nhatanhtruong20@gmail.com>
2025-10-28 11:40:08 +0700

    feat(migration): add quota and session management tables and logic
    - Create alias_daily_summaries and session_daily_progresses tables
    - Remove alias_daily_progresses table

---
+++ PsychologySupport/Services/UserMemory/UserMemory.API/Data/Migrations/20251028005911_AddQuotaAndSessionManagement.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserMemory.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuotaAndSessionManagement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "alias_daily_progresses");
+
+            migrationBuilder.CreateTable(
+                name: "alias_daily_summaries",
+                columns: table => new
+                {
+                    alias_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    date = table.Column<DateOnly>(type: "date", nullable: false),
+                    reward_claim_count = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    last_modified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    last_modified_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_alias_daily_summaries", x => new { x.alias_id, x.date });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "session_daily_progresses",
+                columns: table => new
+                {
+                    alias_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    progress_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    session_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    progress_points = table.Column<int>(type: "integer", nullable: false),
+                    last_increment = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    last_modified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    last_modified_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_session_daily_progresses", x => new { x.alias_id, x.progress_date });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "alias_daily_summaries");
+
+            migrationBuilder.DropTable(
+                name: "session_daily_progresses");
+
+            migrationBuilder.CreateTable(
+                name: "alias_daily_progresses",
+                columns: table => new
+                {
+                    alias_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    progress_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    last_increment = table.Column<int>(type: "integer", nullable: false),
+                    last_modified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    last_modified_by = table.Column<string>(type: "text", nullable: true),
+                    progress_points = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_alias_daily_progresses", x => new { x.alias_id, x.progress_date });
+                });
+        }
+    }
+}
