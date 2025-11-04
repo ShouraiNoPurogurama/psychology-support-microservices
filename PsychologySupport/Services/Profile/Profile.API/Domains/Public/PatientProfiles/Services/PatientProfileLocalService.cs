8ed8d5be57b2b6c4562540ab79789743acc2c706
Nhat Anh <nhatanhtruong20@gmail.com>
2025-11-04 07:33:42 +0700

    refactor(patient-profiles): enhance logging and simplify profile retrieval
    - Add logging for invalid subject reference and missing patient profile
    - Simplify patient profile retrieval logic by removing unnecessary joins

--- PsychologySupport/Services/Profile/Profile.API/Domains/Public/PatientProfiles/Services/PatientProfileLocalService.cs
+++ PsychologySupport/Services/Profile/Profile.API/Domains/Public/PatientProfiles/Services/PatientProfileLocalService.cs
@@ -3,36 +3,39 @@ using Profile.API.Domains.Public.PatientProfiles.Dtos;
 
 namespace Profile.API.Domains.Public.PatientProfiles.Services;
 
-public class PatientProfileLocalService(ProfileDbContext profileDbContext, PiiDbContext piiDbContext)
+public class PatientProfileLocalService(
+    ILogger<PatientProfileLocalService> logger,
+    ProfileDbContext profileDbContext, 
+    PiiDbContext piiDbContext)
 {
     // Đổi tên tham số từ aliasId -> subjectRef
     public async Task<SimplePatientProfileDto> GetPublicProfileBySubjectRefAsync(string subjectRef, CancellationToken ct)
     {
         if (!Guid.TryParse(subjectRef, out var subjectRefGuid))
         {
-            return null; // Handle Guid không hợp lệ
+            logger.LogCritical("SubjectRef không hợp lệ: {SubjectRef}", subjectRef);
+            return null; 
         }
         
         // --- BƯỚC 1: Lấy PatientProfileId (Đã đơn giản hóa) ---
-        // Bỏ join với AliasOwnerMaps, chỉ cần tìm trong PatientOwnerMaps
         var patientProfileId = await piiDbContext.PatientOwnerMaps
-            .Where(p => p.SubjectRef == subjectRefGuid) // <-- Lọc trực tiếp
-            .Select(p => p.PatientProfileId) // <-- Chỉ cần select
+            .Where(p => p.SubjectRef == subjectRefGuid) 
+            .Select(p => p.PatientProfileId) 
             .FirstOrDefaultAsync(cancellationToken: ct);
         
-        // Nếu không có patient profile nào được map, trả về null
         if (patientProfileId == Guid.Empty)
         {
+            logger.LogCritical("PatientProfileId không tìm thấy cho SubjectRef: {SubjectRef}", subjectRef);
             return null;
         }
 
-        // --- BƯỚC 2: Lấy Job (Giữ nguyên) ---
         var patientProfile = await profileDbContext.PatientProfiles
             .Include(p => p.Job)
             .Where(p => p.Id == patientProfileId)
-            .Select(p => new SimplePatientProfileDto( p.Job!.JobTitle))
             .FirstOrDefaultAsync(cancellationToken: ct);
 
-        return patientProfile;
+        var profileDto = new SimplePatientProfileDto(patientProfile?.Job?.JobTitle ?? string.Empty);
+
+        return profileDto;
     }
 }
