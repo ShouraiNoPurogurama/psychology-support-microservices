8347aac9e8aaf5845d8b39e38659b0c673d5b57f
nctan123 <canhtancanhtan@gmail.com>
2025-11-09 13:51:50 +0700

    fix(mapping): map all ChallengeStep fields and Activity in ChallengeStepProgressDto

--- PsychologySupport/Services/Wellness/Wellness.Application/Extensions/MapsterConfigurations.cs
+++ PsychologySupport/Services/Wellness/Wellness.Application/Extensions/MapsterConfigurations.cs
@@ -19,16 +19,24 @@ namespace Wellness.Application.Extensions
 
             TypeAdapterConfig<ChallengeStep, ChallengeStepDto>
                 .NewConfig()
-                .Map(dest => dest.Activity, src => src.Activity);
+                .Map(dest => dest.Activity, src => src.Activity.Adapt<ActivityDto>());
 
             TypeAdapterConfig<Challenge, ChallengeDto>
                 .NewConfig()
                 .Map(dest => dest.ChallengeType, src => src.ChallengeType.ToString())
-                .Map(dest => dest.Steps, src => src.ChallengeSteps);
+                .Map(dest => dest.Steps, src => src.ChallengeSteps.Adapt<List<ChallengeStepDto>>());
 
             TypeAdapterConfig<ChallengeStepProgress, ChallengeStepProgressDto>
-                .NewConfig()
-                .Map(dest => dest.Activity, src => src.ChallengeStep.Activity);
+               .NewConfig()
+               .Map(dest => dest.StepId, src => src.ChallengeStepId)
+               .Map(dest => dest.DayNumber, src => src.ChallengeStep.DayNumber)
+               .Map(dest => dest.OrderIndex, src => src.ChallengeStep.OrderIndex)
+               .Map(dest => dest.Activity, src => src.ChallengeStep.Activity)
+               .Map(dest => dest.ProcessStatus, src => src.ProcessStatus)
+               .Map(dest => dest.StartedAt, src => src.StartedAt)
+               .Map(dest => dest.CompletedAt, src => src.CompletedAt)
+               .Map(dest => dest.PostMoodId, src => src.PostMoodId);
         }
     }
+
 }
