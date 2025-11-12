8d53cfd3f86499f7ddf66aed44d367eff32c58b7
Nhat Anh <nhatanhtruong20@gmail.com>
2025-11-13 01:55:45 +0700

    feat(building blocks): add developer account identification logic
    - Implement DevConfigs class to check for developer accounts
    - Define static array of developer subject references

---
+++ PsychologySupport/BuildingBlocks/BuildingBlocks/Constants/DevConfigs.cs
@@ -0,0 +1,14 @@
+﻿namespace BuildingBlocks.Constants;
+
+public class DevConfigs
+{
+    private static readonly string[] DevSubjectRefs =
+    [
+        "765caad0-89ab-44a0-8471-86f76d3a5b04"
+    ];
+
+    public static bool IsDeveloperAccount(Guid subjectRef)
+    {
+        return DevSubjectRefs.Contains(subjectRef.ToString());
+    }
+}
